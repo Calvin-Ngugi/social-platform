e0dc67b8c1063e44a6bd8bd0b1df41267e91b147
Calvin Ngugi <ncalvin67@gmail.com>
2023-07-19 22:06:38 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "410px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      colors: {
+        primary: "#efefef",
+        secondary: "#011122",
+      },
+    },
+  },
+  plugins: [],
+};
