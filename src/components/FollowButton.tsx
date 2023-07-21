63b981ff2c97f2a4d93af226ba000e828391205e
Calvin Ngugi <ncalvin67@gmail.com>
2023-07-21 15:55:29 +0300

    :zap: Following persists on localStorage

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,9 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
+
 const FollowButton = ({ user, loggedInUser }: any) => {
-const [follow, setFollow] = useState(false);
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    // Check if the current user is following the user on component mount
+    setIsFollowing(localStorage.getItem(`follow_${user.id}`) === "true");
+  }, [user.id]);
+
   const handleFollow = () => {
-      setFollow(!follow)
-      localStorage.setItem('follow', JSON.stringify(follow));
+    setIsFollowing(true);
+    localStorage.setItem(`follow_${user.id}`, "true");
+  };
+
+  const handleUnfollow = () => {
+    setIsFollowing(false);
+    localStorage.removeItem(`follow_${user.id}`);
   };
 
   return (
@@ -11,10 +23,10 @@ const [follow, setFollow] = useState(false);
       {loggedInUser ? (
         user.id === loggedInUser.id ? (
           <div></div>
-        ) : follow ? (
+        ) : isFollowing ? (
           <button
             className="bg-slate-500 rounded-lg p-2 hover:bg-slate-700"
-            onClick={handleFollow}
+            onClick={handleUnfollow}
           >
             Following
           </button>
